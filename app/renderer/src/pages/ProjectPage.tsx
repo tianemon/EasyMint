d19b11375e68b6b03f52ecac2d47cd9e65865250
Amon <811978297@qq.com>
2026-07-31 14:02:59 +0800

    fix: TabBar import 遗漏

--- app/renderer/src/pages/ProjectPage.tsx
+++ app/renderer/src/pages/ProjectPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
+import { TabBar } from "../components/TabBar";
 import { EditorPanel } from "../components/EditorPanel";
 import { ChatPanel } from "../components/ChatPanel";
 import { SettingsDialog, type SettingsTab } from "../components/SettingsDialog";
