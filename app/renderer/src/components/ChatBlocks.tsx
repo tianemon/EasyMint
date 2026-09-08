96df3404303480f067686967808e0483072ca578
Amon <811978297@qq.com>
2026-09-08 12:15:33 +0800

    识图工具图标换为 photo-ai

--- app/renderer/src/components/ChatBlocks.tsx
+++ app/renderer/src/components/ChatBlocks.tsx
@@ -65,9 +65,9 @@ function toolIconPaths(name: string): JSX.Element | null {
     // ask(message-circle-question-mark)
     case "ask_user":
       return (<><path d="M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></>);
-    // 图片(photo-search)
+    // 图片(photo-ai)
     case "describe_image":
-      return (<><path d="M15 8h.01"/><path d="M11.5 21h-5.5a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v5.5"/><path d="M15 18a3 3 0 1 0 6 0a3 3 0 1 0-6 0"/><path d="M20.2 20.2l1.8 1.8"/><path d="M3 16l5-5c.928-.893 2.072-.893 3 0l2 2"/></>);
+      return (<><path d="M15 8h.01"/><path d="M10 21h-4a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v5"/><path d="M3 16l5-5c.928-.893 2.072-.893 3 0l1 1"/><path d="M14 21v-4a2 2 0 1 1 4 0v4"/><path d="M14 19h4"/><path d="M21 15v6"/></>);
     default: return null;
   }
 }
